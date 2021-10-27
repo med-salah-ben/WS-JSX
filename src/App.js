ab0770b1f974ba013a200a35e3ce84cecda9a49d
med-salah-ben <med.salah.ben.amou@gmail.com>
2021-10-27 17:12:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+import Comments from './Components/Comments';
+import MainVideo from "./Components/MainVideo"
+import SuggestionVideos from './Components/SuggestionVideos/SuggestionVideos.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+    <div className="row mt-3">    
+      <MainVideo />
+      <SuggestionVideos />
+
     </div>
+     <div>
+       <Comments />
+     </div>
+    </div>
+
   );
 }
 
---
+++ src/Components/MainVideo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const MainVideo = () => {
+    return (
+        <div className="col-md-7">
+        <div className="row">
+         <iframe width="756" height="425" src="https://www.youtube.com/embed/TfHLTVuJ92A" 
+         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write;
+          encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div className="row">
+          <h2>
+              learn jsx and how to use it
+          </h2>
+        </div>
+        </div>
+    )
+}
+
+export default MainVideo
